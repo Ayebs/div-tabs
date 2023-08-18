23d84038dea16c3b3449ba3661b9509ecc1a78f7
Ayebs <korantengsarah33@gmail.com>
2023-08-18 18:25:00 +0000

    First Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const tabs = document.querySelectorAll('.tab')
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        removeClass()
+        tab.classList.add('active')
+    })
+   
+})
+
+function removeClass() {
+    tabs.forEach(tab => {
+        tab.classList.remove('active')
+    })
+}
